dc6b8284fa2c535fa6408724a618687722a45f81
CristoferMar <cristoferm@gmail.com>
2021-07-23 18:03:03 -0700

    added event listener for client profile button

--- client/app.jsx
+++ client/app.jsx
@@ -7,6 +7,17 @@ export default class App extends React.Component {
       drawerOpened: false,
       location: 'My-Lists'
     };
+    this.handleDrawer = this.handleDrawer.bind(this);
+  }
+
+  // componentDidMount() {
+
+  // }
+
+  handleDrawer(event) {
+    let drawerStatus = false;
+    this.state.drawerOpened ? drawerStatus = false : drawerStatus = true;
+    this.setState({ drawerOpened: drawerStatus });
   }
 
   render() {
@@ -17,9 +28,16 @@ export default class App extends React.Component {
         <img className="small-logo click" src="images/small-logo.svg" alt="small logo 1, 2, Date" />
         <div className="center-content space-between">
           <p className="blue weight-800 center-content align-center">{title}</p>
-          <div className="center-content user-circle click">U</div>
+          <div className="center-content user-circle click" onClick={this.handleDrawer}>U</div>
+        </div>
+        <div className="absolute">
+          {
+            this.state.drawerOpened &&
+            <div>Hello Words!</div>
+          }
         </div>
       </div>
+
     );
   }
 }
