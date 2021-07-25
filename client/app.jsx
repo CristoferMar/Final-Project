c83eddef6e7e172bb96ac5014da5d287862dec4a
CristoferMar <cristoferm@gmail.com>
2021-07-24 21:35:33 -0700

    Added expandable and contractable full navigation drawer

--- client/app.jsx
+++ client/app.jsx
@@ -4,7 +4,7 @@ export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      drawerOpened: false,
+      drawerOpened: 'drawer-close',
       location: 'My-Lists'
     };
     this.handleDrawer = this.handleDrawer.bind(this);
@@ -15,8 +15,8 @@ export default class App extends React.Component {
   // }
 
   handleDrawer(event) {
-    let drawerStatus = false;
-    this.state.drawerOpened ? drawerStatus = false : drawerStatus = true;
+    let drawerStatus = '';
+    this.state.drawerOpened === 'drawer-open' ? drawerStatus = 'drawer-close' : drawerStatus = 'drawer-open';
     this.setState({ drawerOpened: drawerStatus });
   }
 
@@ -24,20 +24,29 @@ export default class App extends React.Component {
     let title = this.state.location;
     title = title.replace('-', ' ');
     return (
-      <div className='nav-bar'>
-        <img className="small-logo click" src="images/small-logo.svg" alt="small logo 1, 2, Date" />
-        <div className="center-content space-between">
-          <p className="blue weight-800 center-content align-center">{title}</p>
-          <div className="center-content user-circle click" onClick={this.handleDrawer}>U</div>
+      <>
+        <div className='nav-bar'>
+          <img className="small-logo click" src="images/small-logo.svg" alt="small logo 1, 2, Date" />
+          <div className="center-content space-between">
+            <p className="blue weight-800 center-content align-center">{title}</p>
+            <div className="center-content user-circle click" onClick={this.handleDrawer}>U</div>
+          </div>
+          <div className={'absolute ' + this.state.drawerOpened}>
+            <p className="click">Generate Date</p>
+            <p className="click">My Lists</p>
+            <p className="click">My History</p>
+            <p className="click">What&apos;s Poppin</p>
+            <p><hr className="solid" /></p>
+            <p className="red click">Sign Out</p>
+          </div>
         </div>
-        <div className="absolute">
-          {
-            this.state.drawerOpened &&
-            <div>Hello Words!</div>
-          }
-        </div>
-      </div>
 
+        {/* navbar above ^ */}
+
+        <div className="page align-center">
+
+        </div>
+      </>
     );
   }
 }
