d68cf533af852f3dba421f21a89ef8c55ff805e3
CristoferMar <cristoferm@gmail.com>
2021-07-29 10:33:11 -0700

    Add event listener to "My-Lists" page on individual list number

--- client/components/user-lists.jsx
+++ client/components/user-lists.jsx
@@ -6,6 +6,13 @@ export default class UserLists extends React.Component {
     this.state = {
       userLists: []
     };
+    this.handleListClick = this.handleListClick.bind(this);
+  }
+
+  handleListClick(event) {
+    // console.log('event.target:', event.target);
+    const listSelected = event.target.getAttribute('id');
+    // console.log('listSelected:', listSelected);
   }
 
   componentDidMount() {
@@ -23,27 +30,29 @@ export default class UserLists extends React.Component {
 
   render() {
     return (
-        <div className="padding-10 width-responsive">
+      <div className="padding-10 width-responsive">
         <div className="flex full-width align-center space-between padding-vert-10n25">
-            <h3>Create A New List</h3>
-            <a href="#New-List" className="flex align-center">
-              <img className="height-25 click" src="images/add-list-btn.svg" alt="add new list" />
-            </a>
-          </div>
+          <h3>Create A New List</h3>
+          <a href="#New-List" className="flex align-center">
+            <img className="height-25 click" src="images/add-list-btn.svg" alt="add new list" />
+          </a>
+        </div>
 
-          {
-            this.state.userLists.length === 0 &&
-              <div className="content-center full-width padding-10">
-                  You have no lists at the moment.
-              </div>
-          }
-          {
-            this.state.userLists.length > 0 &&
-            this.state.userLists.map(listItem =>
+        {
+          this.state.userLists.length === 0 &&
+          <div className="content-center full-width padding-10">
+            You have no lists at the moment.
+          </div>
+        }
+        {
+          this.state.userLists.length > 0 &&
+          this.state.userLists.map(listItem =>
             <div key={listItem.listId} className="padding-top-5">
               <div className="flex full-width space-between">
-                <p className="width-76-percent click">{listItem.listTitle}</p>
-                  <div className="center-content align-center max-height-31">
+                <p onClick={this.handleListClick} id={listItem.listId} className="width-76-percent click">
+                  {listItem.listTitle}
+                </p>
+                <div className="center-content align-center max-height-31">
                   <p className="font-light-responsive center-content align-center">
                     {2500 + ' Items'}
                   </p>
@@ -51,10 +60,10 @@ export default class UserLists extends React.Component {
                 </div>
               </div>
             </div>
-            )
-          }
+          )
+        }
 
-        </div>
+      </div>
     );
   }
 }
