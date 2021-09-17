09bfd104cf676bcba407c5437abe6904880f718a
CristoferMar <cristoferm@gmail.com>
2021-09-16 17:47:09 -0700

    addead loading animation to history screen

--- client/components/user-history.jsx
+++ client/components/user-history.jsx
@@ -4,28 +4,52 @@ export default class UserHistory extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      history: []
+      history: [],
+      historyLoaded: false
     };
 
   }
 
   componentDidMount() {
-    const req = {
-      method: 'GET',
-      header: { 'Content-Type': 'application/json' }
-    };
-    fetch('api/history', req)
-      .then(res => res.json())
-      .then(userHistory => {
-        this.setState({ history: userHistory });
-        // console.log(this.state);
-      });
+    // const req = {
+    //   method: 'GET',
+    //   header: { 'Content-Type': 'application/json' }
+    // };
+    // fetch('api/history', req)
+    //   .then(res => res.json())
+    //   .then(userHistory => {
+    //     this.setState({ history: userHistory, historyLoaded: true });
+    //     console.log(this.state);
+    //   });
   }
 
   render() {
     return (
       <>
-        <div className="padding-10 width-responsive">My History</div>
+        <div className="padding-10 width-responsive">
+          <div className="full-width center-content padding-vert-10n25">
+            <h3>My History</h3>
+          </div>
+          {this.state.historyLoaded
+            ? <>
+                {
+                  !this.state.history.length
+                    ? <>
+                    <p className="form-title">It seems you haven&apos;t chosen to do any of your dates/items.</p>
+                    <p className="form-title">When you generate a item then hit &quot;Let&apos;s Do This&quot;, the items will appear here, with the most recent item at the top.</p>
+                  </>
+                    : <>
+                    <div className="full-width">Yay</div>
+                  </>
+                }
+              </>
+            : <>
+              <div className="full-width center-content">
+                <div className="lds-dual-ring"></div>
+              </div>
+            </>
+          }
+        </div>
       </>
     );
   }
