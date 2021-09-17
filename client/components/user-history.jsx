2bb7e05056d139b65e64bfa824465667bcf8706f
CristoferMar <cristoferm@gmail.com>
2021-09-17 13:29:40 -0700

    created and added time elapsed calculator function for history

--- client/components/user-history.jsx
+++ client/components/user-history.jsx
@@ -5,22 +5,35 @@ export default class UserHistory extends React.Component {
     super(props);
     this.state = {
       history: [],
-      historyLoaded: false
+      historyLoaded: false,
+      currentTime: new Date()
     };
-
+    this.calculateTime = this.calculateTime.bind(this);
   }
 
   componentDidMount() {
-    // const req = {
-    //   method: 'GET',
-    //   header: { 'Content-Type': 'application/json' }
-    // };
-    // fetch('api/history', req)
-    //   .then(res => res.json())
-    //   .then(userHistory => {
-    //     this.setState({ history: userHistory, historyLoaded: true });
-    //     console.log(this.state);
-    //   });
+    const req = {
+      method: 'GET',
+      header: { 'Content-Type': 'application/json' }
+    };
+    fetch('api/history', req)
+      .then(res => res.json())
+      .then(userHistory => {
+        this.setState({ history: userHistory, historyLoaded: true });
+        // console.log(this.state);
+        // console.log(this.state.history[0].addedAt);
+      });
+  }
+
+  // still need to work out the second / minute / day calculations
+  calculateTime(timeString) {
+    const oldTime = new Date(timeString);
+    const elapsedSeconds = (this.state.currenttime - oldTime) / 1000;
+    if (elapsedSeconds < 60) return `${Math.floor(elapsedSeconds)} s ago`;
+    if (elapsedSeconds < 3600) return `${Math.floor(elapsedSeconds / 60)} m ago`;
+    if (elapsedSeconds < 86400) return `${Math.floor(elapsedSeconds / 3600)} h ago`;
+    if (elapsedSeconds < 2592000) return `${Math.floor(elapsedSeconds / 86400)} d ago`;
+    return oldTime.toLocaleString();
   }
 
   render() {
@@ -35,19 +48,21 @@ export default class UserHistory extends React.Component {
                 {
                   !this.state.history.length
                     ? <>
-                    <p className="form-title">It seems you haven&apos;t chosen to do any of your dates/items.</p>
-                    <p className="form-title">When you generate a item then hit &quot;Let&apos;s Do This&quot;, the items will appear here, with the most recent item at the top.</p>
-                  </>
+                      <p className="form-title">It seems you haven&apos;t chosen to do any of your dates/items.</p>
+                      <p className="form-title">When you generate a item then hit &quot;Let&apos;s Do This&quot;, the items will appear here, with the most recent item at the top.</p>
+                    </>
                     : <>
-                    <div className="full-width">Yay</div>
-                  </>
+                      <div className="full-width">
+                          <div>
+                            Used 3 of Clubs from Deck of Cards
+                          </div>
+                      </div>
+                    </>
                 }
               </>
-            : <>
-              <div className="full-width center-content">
+            : <div className="full-width center-content">
                 <div className="lds-dual-ring"></div>
               </div>
-            </>
           }
         </div>
       </>
