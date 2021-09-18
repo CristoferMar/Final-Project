2611e699e7d60ac13acff61987151c11cb326121
CristoferMar <cristoferm@gmail.com>
2021-09-17 19:12:25 -0700

    cleaned up commented code | adjusted font size responsiveness

--- client/components/user-history.jsx
+++ client/components/user-history.jsx
@@ -49,7 +49,7 @@ export default class UserHistory extends React.Component {
                     : <>
                         {this.state.history.map(item =>
                           <div className="full-width flex space-between" key={item.addedAt}>
-                            <div className="margin-bottom-10 font-1rm">
+                            <div className="margin-bottom-10 font-1rm-res">
                               <p><i>{item.dateIdea}</i></p>
                               <p>from <i>{item.listTitle}</i></p>
                             </div>
