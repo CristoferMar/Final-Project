c4a6e62ce878d22ac7bc5a20184fb401d84108d7
CristoferMar <cristoferm@gmail.com>
2021-09-26 20:34:33 -0700

    add needed import and export material for React

---
+++ client/components/landing-page.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default class LandingPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: 'cris'
+    };
+  }
+
+  render() {
+    return (
+      <>
+      </>
+    );
+  }
+
+}
