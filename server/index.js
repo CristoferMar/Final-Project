33177ff669f357c1abafbda239d1d7aa7c470c89
CristoferMar <cristoferm@gmail.com>
2021-10-13 00:26:11 -0700

    rearranged index.js to set limitations if a user is not authorized

--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@ const errorMiddleware = require('./error-middleware');
 const staticMiddleware = require('./static-middleware');
 const argon2 = require('argon2');
 const jwt = require('jsonwebtoken');
+const authorizationMiddleware = require('./authorization-middleware');
 
 const db = new pg.Pool({
   connectionString: process.env.DATABASE_URL,
@@ -17,9 +18,79 @@ const db = new pg.Pool({
 const app = express();
 
 const jsonMiddleware = express.json();
+
 app.use(jsonMiddleware);
 
 app.use(staticMiddleware);
+
+app.post('/api/auth/sign-up', (req, res, next) => {
+  const { username, password } = req.body;
+  if (!username || !password) {
+    throw new ClientError(400, 'Username and password are required fields.');
+  }
+
+  argon2.hash(password)
+    .then(hash => {
+      const sql = `
+      insert into
+      "users" ("userName", "password")
+      values ($1, $2)
+      on conflict ("userName")
+      do nothing
+      returning "userId", "userName", "createdAt"
+      `;
+      const params = [username, hash];
+      db.query(sql, params)
+        .then(result => {
+          if (result.rows[0]) {
+            res.status(201).json(result.rows[0]);
+          } else {
+            throw new ClientError(409, 'This user name is already taken.');
+          }
+          res.status(201).json(result.rows[0]);
+        })
+        .catch(err => next(err));
+    })
+    .catch(err => next(err));
+});
+
+app.post('/api/auth/sign-in', (req, res, next) => {
+  const { username, password } = req.body;
+  if (!username || !password) {
+    throw new ClientError(400, 'Username and password are required fields.');
+  }
+
+  const sql = `
+  select "userId", "password"
+  from "users"
+  where "userName" = $1
+  `;
+  const params = [username];
+
+  db.query(sql, params)
+    .then(response => {
+      if (response.rowCount < 1) {
+        throw new ClientError(401, 'invalid login');
+      }
+
+      argon2.verify(response.rows[0].password, password)
+        .then(matchTest => {
+          if (!matchTest) {
+            throw new ClientError(401, 'invalid login');
+          } else {
+            const payload = { userId: response.rows[0].userId, username: username };
+            const token = jwt.sign(payload, process.env.TOKEN_SECRET);
+            const wholeResponse = { token: token, user: payload };
+            // console.log('wholeResponse:', wholeResponse);
+            res.status(200).json(wholeResponse);
+          }
+        })
+        .catch(err => next(err));
+    })
+    .catch(err => next(err));
+});
+
+app.use(authorizationMiddleware);
 
 app.post('/api/lists', (req, res, next) => {
   const { listName } = req.body;
@@ -195,73 +266,6 @@ app.patch('/api/dateActive/:dateId', (req, res, next) => {
   db.query(sql, params)
     .then(result => {
       res.sendStatus(204);
-    })
-    .catch(err => next(err));
-});
-
-app.post('/api/auth/sign-up', (req, res, next) => {
-  const { username, password } = req.body;
-  if (!username || !password) {
-    throw new ClientError(400, 'Username and password are required fields.');
-  }
-
-  argon2.hash(password)
-    .then(hash => {
-      const sql = `
-      insert into
-      "users" ("userName", "password")
-      values ($1, $2)
-      on conflict ("userName")
-      do nothing
-      returning "userId", "userName", "createdAt"
-      `;
-      const params = [username, hash];
-      db.query(sql, params)
-        .then(result => {
-          if (result.rows[0]) {
-            res.status(201).json(result.rows[0]);
-          } else {
-            throw new ClientError(409, 'This user name is already taken.');
-          }
-          res.status(201).json(result.rows[0]);
-        })
-        .catch(err => next(err));
-    })
-    .catch(err => next(err));
-});
-
-app.post('/api/auth/sign-in', (req, res, next) => {
-  const { username, password } = req.body;
-  if (!username || !password) {
-    throw new ClientError(400, 'Username and password are required fields.');
-  }
-
-  const sql = `
-  select "userId", "password"
-  from "users"
-  where "userName" = $1
-  `;
-  const params = [username];
-
-  db.query(sql, params)
-    .then(response => {
-      if (response.rowCount < 1) {
-        throw new ClientError(401, 'invalid login');
-      }
-
-      argon2.verify(response.rows[0].password, password)
-        .then(matchTest => {
-          if (!matchTest) {
-            throw new ClientError(401, 'invalid login');
-          } else {
-            const payload = { userId: response.rows[0].userId, username: username };
-            const token = jwt.sign(payload, process.env.TOKEN_SECRET);
-            const wholeResponse = { token: token, user: payload };
-            // console.log('wholeResponse:', wholeResponse);
-            res.status(200).json(wholeResponse);
-          }
-        })
-        .catch(err => next(err));
     })
     .catch(err => next(err));
 });
