eca01956d1b934add29f2d85ee3bda0596254a77
CristoferMar <cristoferm@gmail.com>
2021-11-15 21:44:24 -0800

    added loading animation list-details page | corrected rendering order of components to never show premature API cal results

--- client/components/list-details.jsx
+++ client/components/list-details.jsx
@@ -6,7 +6,8 @@ export default class ListDetails extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      listData: null
+      listData: {},
+      dataHasLoaded: false
     };
   }
 
@@ -20,7 +21,7 @@ export default class ListDetails extends React.Component {
     };
     fetch(`/api/dates/${this.props.listId}`, req)
       .then(res => res.json())
-      .then(listData => this.setState({ listData }))
+      .then(listData => this.setState({ listData, dataHasLoaded: true }))
       .catch(err => console.error(err));
   }
 
@@ -28,7 +29,7 @@ export default class ListDetails extends React.Component {
     let listTitle = null;
     let dateIdeas = null;
     let listId = null;
-    if (this.state.listData) {
+    if (this.state.listData.listTitle) {
       listTitle = this.state.listData.listTitle;
       dateIdeas = this.state.listData.dateIdeas;
       listId = this.state.listData.listId;
@@ -36,51 +37,59 @@ export default class ListDetails extends React.Component {
 
     return (
       <div className="padding-10 width-responsive">
-       {
-          listTitle &&
+
+        {this.state.dataHasLoaded &&
           <>
-            <div className="flex space-between full-width align-center padding-vert-10n25">
-              <a href="#My-Lists" className="flex align-center float-left">
-                <img className="height-25 click" src="/images/tri-colored-back-arrow.svg" alt="add new list" />
-              </a>
-              <h3 className="center-content align-center">{listTitle}</h3>
-              <div className="width-25"></div>
-            </div>
-            <div className="center-content align-center space-between margin-bottom-10">
-              <h4>
-                Add Date/Item?
-              </h4>
-                <a href={`#New-Date?listId=${listId}`}><img className="height-25 click" src="/images\add-date-btn.svg" alt="add date or item" /></a>
-            </div>
+            {listTitle
+              ? <>
+                <div className="flex space-between full-width align-center padding-vert-10n25">
+                  <a href="#My-Lists" className="flex align-center float-left">
+                    <img className="height-25 click" src="/images/tri-colored-back-arrow.svg" alt="add new list" />
+                  </a>
+                  <h3 className="center-content align-center">{listTitle}</h3>
+                  <div className="width-25"></div>
+                </div>
+                <div className="center-content align-center space-between margin-bottom-10">
+                  <h4>
+                    Add Date/Item?
+                  </h4>
+                  <a href={`#New-Date?listId=${listId}`}><img className="height-25 click" src="/images\add-date-btn.svg" alt="add date or item" /></a>
+                </div>
+              </>
+              : <>
+                <div className="padding-vert-10n25">This list ID is not available</div>
+                <a href="#My-Lists" className="center-content align-center">
+                  <img className="height-25 click" src="/images/tri-colored-back-arrow.svg" alt="add new list" />
+                  <span className="margin-left-5">Return to My Lists?</span>
+                </a>
+              </>
+            }
+
+            {dateIdeas
+              ? <>
+                  {dateIdeas.map(item =>
+                  <div key={item.dateId} id={item.dateId} className="padding-top-5">
+                    <ListItem dateInfo={item} token={this.context.token.token} />
+                  </div>)}
+                </>
+              : dateIdeas && !dateIdeas[0]
+                ? <>
+                  <div className="content-center full-width padding-10">
+                    You have no dates or items in this list.
+                  </div>
+                </>
+                : <> </>
+            }
           </>
-        }
-        {
-          dateIdeas && !dateIdeas[0] &&
-            <div className="content-center full-width padding-10">
-              You have no dates or items in this list.
-            </div>
-        }
-        {
-          dateIdeas && dateIdeas[0] &&
-            dateIdeas.map(item =>
-              <div key={item.dateId} id={item.dateId} className="padding-top-5">
-                <ListItem dateInfo={item} token={this.context.token.token} />
-              </div>
-            )
-        }
-        {
-          listTitle === undefined &&
-            <>
-              <div className="padding-vert-10n25">This list ID is not available</div>
-              <a href="#My-Lists" className="center-content align-center">
-                <img className="height-25 click" src="/images/tri-colored-back-arrow.svg" alt="add new list" />
-                <span className="margin-left-5">Return to My Lists?</span>
-              </a>
-            </>
-        }
+         }
+         {!this.state.dataHasLoaded &&
+          <div className="full-width center-content">
+            <div className="lds-dual-ring"></div>
+          </div>
+         }
+
       </div>
     );
   }
 }
-
 ListDetails.contextType = AppContext;
